4dd12bb76694d02dcbfe2759a4950d1bd54f44a5
Chad <chdabney42@gmail.com>
2020-07-09 15:28:41 -0400

    done

---
+++ assets/scripts/coinWithClass.js
@@ -0,0 +1,14 @@
+class Coin {
+    constructor(initialState) {
+        this.state = initialState
+    }
+    flip() {
+        console.log("coin.flip() has been invoked.")
+    }
+    toString() {
+        console.log("coin.toString() has been invoked.")
+    }
+    toHTML() {
+        console.log("coin.toHTML() has been invoked.")
+    }
+}
